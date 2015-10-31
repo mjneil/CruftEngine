3f3633e77d5d024b01eb5add9574328e5758bba3
Matthew Neil <mneilweb@gmail.com>
2015-10-30 21:20:32 -0500

    added start of Geometry.js

---
+++ engine/graphics/Geometry.js
@@ -0,0 +1,24 @@
+export default class Geometry {
+    constructor() {
+        this.vertices = null;
+        this.texCoords = null;
+        this.normals = null;
+        this.faces = null;
+    }
+
+    get hasVertices() {
+        return !(this.vertices === null);
+    }
+
+    get hasNormals() {
+        return !(this.normals === null);
+    }
+
+    get hasTexCoords() {
+        return !(this.texCoords === null);
+    }
+}
+
+
+
+
