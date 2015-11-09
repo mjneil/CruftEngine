cda557529dfc6a7155bdc2efd799d6bea3b2a6f2
Matt Neil <mneil@wi.rr.com>
2015-11-09 17:26:34 -0600

    Add comments to Emitter class

--- engine/core/Emitter.js
+++ engine/core/Emitter.js
@@ -1,11 +1,19 @@
 const EVENTS = new WeakMap();
 
 export default class Emitter {
-
+    /**
+     * Constructor for Emitter class
+     */
     constructor() {
         EVENTS.set(this, []);
     }
 
+    /**
+     * Subscribe the given callbacks to the specified event topic(s).
+     * @param  {String}     topic    Space seperated list of topics.
+     * @param  {...Array}   callback List of callbacks to subscribe.
+     * @return {Emitter}             Returns self to allow chain calling.
+     */
     on(topic, ...callback) {
         let e = EVENTS.get(this);
         let topics = topic.split(" ");
@@ -23,6 +31,12 @@ export default class Emitter {
         return this;
     }
 
+    /**
+     * Unsubscribe the given callbacks from the sepcified event topic(s).
+     * @param  {String}     topic    Space seperated list of topics.
+     * @param  {...Array}   callback List of callbacks to unsubscribe.
+     * @return {Emitter}             Returns self to allow chain calling.
+     */
     off(topic, ...callback) {
         let e = EVENTS.get(this);
         let topics = topic.split(" ");
@@ -40,6 +54,12 @@ export default class Emitter {
         return this;
     }
 
+    /**
+     * Calls all subscribed functions of the specified event topic.
+     * @param  {String}     topic Event topic to be emitted.
+     * @param  {...Array}   args  List of arguments to be passed to callbacks.
+     * @return {Emitter}          Returns self to allow chain calling.
+     */
     emit(topic, ...args) {
         let e = EVENTS.get(this);
 
