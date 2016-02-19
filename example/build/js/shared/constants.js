abd5f8fc464424f751ec50a2713502bc4a3682d2
Yusef Sohail <yusefsohail@gmail.com>
2016-02-19 13:32:06 -0600

    removing build/ folder

--- example/build/js/shared/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var PEERJS_API_KEY = "vu5babwam1tnjyvi";
-Object.defineProperties(module.exports, {
-  PEERJS_API_KEY: {get: function() {
-      return PEERJS_API_KEY;
-    }},
-  __esModule: {value: true}
-});
